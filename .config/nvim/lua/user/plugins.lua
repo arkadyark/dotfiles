3bcfab8456d919492dfb8bfb5489c48d0581bc2c
Arkady Arkhangorodsky <aarkhangorodsky@gmail.com>
2023-09-09 04:27:07 -0400

    Update nvim deps

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -46,7 +46,7 @@ return packer.startup(function(use)
 
   -- Comments
   use { "numToStr/Comment.nvim", commit = "97a188a98b5a3a6f9b1b850799ac078faa17ab67" }
-  use { "JoosepAlviste/nvim-ts-context-commentstring", commit = "4d3a68c41a53add8804f471fcc49bb398fe8de08" }
+  use { "JoosepAlviste/nvim-ts-context-commentstring" }
 
   -- File tree navigation
   use { "kyazdani42/nvim-web-devicons", commit = "563f3635c2d8a7be7933b9e547f7c178ba0d4352" }
